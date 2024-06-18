ae62487ec9a48b2c7574be3f0380275e96f29ae1
Egor Lugovkin <e.lugovkin@makeroi.ru>
2024-06-18 23:51:56 +0700

    init

--- App/CsvReader.php
+++ App/CsvReader.php
@@ -3,17 +3,22 @@
 namespace App;
 
 use App\Models\User;
+use Exception;
 
 class CSVReader {
 
     public function __construct(protected string $filePath, protected string $delimiter) {
     }
 
+    /**
+     * @throws \Exception
+     */
     public function getUsers(): array
     {
         $users = [];
         if (($handle = fopen($this->filePath, 'r')) !== FALSE) {
             while (($data = fgetcsv($handle, 1000, $this->delimiter)) !== FALSE) {
+                if(count($data) < 2) throw new Exception('Неверный формат данных');
                 $id = $data[0];
                 $users[$id] = new User($id, $data[1]);
             }
